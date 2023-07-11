7ad0a7eb59579a088639c4d8f6a17384a977deeb
Manoj Rawat <positive.mind.123456789@gmail.com>
2023-07-11 16:46:27 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cors({
 
 // Enable CORS for all origins
 app.use((req: Request, res: Response, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
